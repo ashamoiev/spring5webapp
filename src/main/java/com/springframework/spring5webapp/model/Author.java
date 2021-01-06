0aba36c73a28d77909d734afd6cd73392ff4185c
Shamoiev <wamoev@gmail.com>
2021-01-06 20:48:35 +0200

    add entities

--- src/main/java/com/springframework/spring5webapp/model/Author.java
+++ src/main/java/com/springframework/spring5webapp/model/Author.java
@@ -1,11 +1,17 @@
 package com.springframework.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany
     private Set<Book> books = new HashSet<>();
 }
--- src/main/java/com/springframework/spring5webapp/model/Book.java
+++ src/main/java/com/springframework/spring5webapp/model/Book.java
@@ -1,12 +1,18 @@
 package com.springframework.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
     private Set<Author> authors = new HashSet<>();
 }
